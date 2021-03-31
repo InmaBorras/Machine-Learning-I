d5a4997163a48ae48b09f3cc0ce82acf17c1137b
iborras <i.borras.2020@alumnos.urjc.es>
2021-03-31 16:33:46 +0200

    Jupyterbook

---
+++ MachinelearningI/_build/jupyter_execute/Aprendizaje_supervisado.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Aprendizaje Supervisado 
+# 
+# ## GLM : Regresion Logistica 
+# 
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+
+
+# In[2]:
+
+
+# Create logistic regression model
+model = LogisticRegression()
+model.fit(#poner aqui las variables )
+
+
+# 
+# ```{toctree}
+# :hidden:
+# :titlesonly:
+# 
+# 
+# Precio
+# ```
+# 
